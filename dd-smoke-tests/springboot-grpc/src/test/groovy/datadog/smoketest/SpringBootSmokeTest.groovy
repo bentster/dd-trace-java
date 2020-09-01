d8982d75ae715e9683b959375c504dba6df93dd0
Richard Startin <richard.startin@datadoghq.com>
2020-09-01 19:25:52 +0100

    make spring boot/grpc test fail when tracing fails

--- dd-smoke-tests/springboot-grpc/src/test/groovy/datadog/smoketest/SpringBootSmokeTest.groovy
+++ dd-smoke-tests/springboot-grpc/src/test/groovy/datadog/smoketest/SpringBootSmokeTest.groovy
@@ -1,8 +1,12 @@
 package datadog.smoketest
 
 import okhttp3.Request
+import spock.lang.Shared
 
 class SpringBootSmokeTest extends AbstractServerSmokeTest {
+
+  @Shared
+  static File output = File.createTempFile("trace-structure", "out")
 
   @Override
   ProcessBuilder createProcessBuilder() {
@@ -11,9 +15,24 @@ class SpringBootSmokeTest extends AbstractServerSmokeTest {
     List<String> command = new ArrayList<>()
     command.add(javaPath())
     command.addAll(defaultJavaProperties)
-    command.addAll((String[]) ["-jar", springBootShadowJar, "--server.port=${httpPort}", "-Ddd.writer.type=LoggingWriter"])
+    command.addAll((String[]) ["-Ddd.writer.type=TraceStructureWriter:${output.getAbsolutePath()}", "-jar", springBootShadowJar, "--server.port=${httpPort}"])
     ProcessBuilder processBuilder = new ProcessBuilder(command)
     processBuilder.directory(new File(buildDirectory))
+  }
+
+
+  def cleanupSpec() {
+    // check the structures written out to the log
+    BufferedReader reader = new BufferedReader(new FileReader(output))
+    try {
+      String line = null
+      while (null == line || !line.isEmpty()) {
+        line = reader.readLine()
+        assert "[servlet.request[spring.handler[grpc.client[grpc.message]]]]".equalsIgnoreCase(line)
+      }
+    } finally {
+      reader.close()
+    }
   }
 
   def "greeter #n th time"() {
